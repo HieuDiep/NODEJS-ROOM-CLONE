0bc57c06ed3cb65bc072bf4bc0de63c282519eec
Diệp Văn Hiếu <41156680+HieuZenKa@users.noreply.github.com>
2020-09-26 14:08:53 +0700

    Add chat and unmute

--- public/script.js
+++ public/script.js
@@ -31,6 +31,22 @@ navigator.mediaDevices.getUserMedia({
     socket.on('user-connected',(userId)=>{
         connecToNewUser(userId,stream);
     })
+    let text = $('input');
+
+    $('html').keydown((e)=>{
+        if(e.which == 13 && text.val().length!==0){
+            socket.emit('message',text.val())
+            text.val('')
+        }
+    })
+
+    socket.on('createMessage', message => {
+        console.log(message)
+        $('.messages').append(`<li class="message"><b>user: </b>${message}</li>`)
+        scrollToBottom();
+    })
+
+    
 })
 
 peer.on('open',id =>{
@@ -53,4 +69,37 @@ const addVideoStream = (video, stream)=>{
         video.play();
     })
     videoGrid.append(video);
+}
+
+const scrollToBottom = ()=>{
+    let d = $ ('.main__chat__window');
+    d.scrollTop(d.prop("scrollHeight"));
+}
+
+const muteUnmute =()=>{
+    const enabled = myVideoStream.getAudioTracks()[0].enabled;
+    if(enabled){
+        myVideoStream.getAudioTracks()[0].enabled = false;
+        setUnmuteButton();
+    }else{
+        setMuteButton();
+        myVideoStream.getAudioTracks()[0].enabled = true;
+    }
+
+}
+
+const setMuteButton = ()=>{
+    const html = `
+    <i class="fas fa-microphone"></i>
+    <span>Mute</span>
+    `
+    document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+const setUnmuteButton = ()=>{
+    const html = `
+    <i class="unmute fas fa-microphone-slash"></i>
+    <span>Unmute</span>
+    `
+    document.querySelector('.main__mute_button').innerHTML = html;
 }
